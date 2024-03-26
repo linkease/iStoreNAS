59d295185672fa53c5dbabc01ed0f766502272dd
ziguayungui <244753440@qq.com>
2024-03-26 14:52:34 +0000

    files: add nginx config file

--- src/multi.mk
+++ src/multi.mk
@@ -32,7 +32,8 @@ image_multi:
 	$(MAKE) -s _check_profile
 	$(MAKE) -s _check_keys
 	(unset PROFILE FILES PACKAGES MAKEFLAGS; \
-	$(MAKE) -f multi.mk -s _call_image_multi )
+	$(MAKE) -f multi.mk -s _call_image_multi \
+		$(if $(FILES),USER_FILES="$(FILES)") )
 
 profiles_multi:
 	@$(STAGING_DIR_HOST)/bin/sed -n 's/^CONFIG_TARGET_$(if $(CONFIG_TARGET_MULTI_PROFILE),DEVICE_)$(call target_conf,$(BOARD)$(if $(SUBTARGET),_$(SUBTARGET)))_\(.*\)=y/\1/p' .config
